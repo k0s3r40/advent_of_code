1f0e7b9dc6978d5f08167e2484d32bdf4ed15a9c
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-03 17:12:12 +0200

    Simplify day 3

--- 2020/3/1.py
+++ 2020/3/1.py
@@ -1,18 +1,16 @@
-import numpy
+import numpy as np
+
+
 def solve(data, right, down):
     x_pos = 0
     trees = 0
-
     for index, i in enumerate(data):
-        if index == 0:
+        if index % down == 0:
+            if i[x_pos] == '#':
+                trees += 1
             x_pos += right
-        else:
-
-            if down != 2 or index % down == 0:
-                if i[x_pos] == '#':
-                    trees += 1
-                x_pos += right
     return trees
+
 
 if __name__ == '__main__':
     with open('input.txt') as f:
@@ -20,5 +18,5 @@ if __name__ == '__main__':
         for x, v in enumerate(data):
             data[x] = [i for i in v * len(data)]
     slopes = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
-    print('part1',solve(data, 3, 1))
-    print('part2', numpy.prod([solve(data, i[0], i[1]) for i in slopes]))
+    print('part1', solve(data, 3, 1))
+    print('part2', np.prod([solve(data, i[0], i[1]) for i in slopes]))
