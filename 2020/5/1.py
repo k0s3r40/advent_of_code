e210aa095d741c83eaf37442dc2dbe058f5aac28
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-05 19:56:02 +0200

    Make it to one liner just for fun

--- 2020/5/1.py
+++ 2020/5/1.py
@@ -1,6 +1,2 @@
 if __name__ == '__main__':
-    with open('input.txt') as f:
-        data = f.read().split('\n')
-    trans ={70: '0', 66: '1', 76: '0', 82: '1'}
-    data = [int(i.translate(trans), 2) for i in data]
-    print(max(data), [i for i in range(min(data), max(data)) if i-1 in data and i+1 in data and i not in data][0])
+    print(next(f'part 1 :{k}\npart 2 :{v}' for k, v in {max(data):next(i for i in range(min(data), max(data)) if i - 1 in data and i + 1 in data and i not in data) for data in [[int(x.translate({70: '0', 66: '1', 76: '0', 82: '1'}), 2) for x in open('input.txt').read().split('\n')]]}.items()))
