fe1155dc4b0f6441fc25d7f29be45f7ceb80f40b
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-05 16:54:11 +0200

    Use trans instead of replace

--- 2020/5/1.py
+++ 2020/5/1.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
     with open('input.txt') as f:
         data = f.read().split('\n')
-    data = [int(i.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2) for i in data]
+    trans ={70: '0', 66: '1', 76: '0', 82: '1'}
+    data = [int(i.translate(trans), 2) for i in data]
     print(max(data), [i for i in range(min(data), max(data)) if i-1 in data and i+1 in data and i not in data][0])
