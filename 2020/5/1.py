63559e6154f48a525eec70a8759d2e1b2b6f2260
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-05 16:53:41 +0200

    Revert "use translate instead of replace"
    This reverts commit fc87fe16

--- 2020/5/1.py
+++ 2020/5/1.py
@@ -1,6 +1,5 @@
 if __name__ == '__main__':
     with open('input.txt') as f:
         data = f.read().split('\n')
-    trans ={70: '0', 66: '1', 76: '0', 82: '1'}
-    data = [int(i.translate(trans), 2) for i in data]
+    data = [int(i.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2) for i in data]
     print(max(data), [i for i in range(min(data), max(data)) if i-1 in data and i+1 in data and i not in data][0])
