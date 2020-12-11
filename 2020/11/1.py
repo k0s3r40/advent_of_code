76a04b6282077fa8a494be136f0fac1d9eadc6a5
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-11 23:30:33 +0200

    Solve part 2

--- 2020/11/1.py
+++ 2020/11/1.py
@@ -5,41 +5,37 @@ def solve_part_1(d, iterations=0, seated=0):
     for k, v in d.items():
         arr.append(v)
         if len(arr) > 9:
-            print(''.join(arr))
+            # print(''.join(arr))
             arr = []
-    print('----------')
+    # print('----------')
     for k, v in d.items():
-        if iterations == 0:
-            if v != '.':
+        indexes = [(k[0] + 1, k[1]),
+                   (k[0] - 1, k[1]),
+                   (k[0], k[1] + 1),
+                   (k[0], k[1] - 1),
+                   (k[0] + 1, k[1] - 1),
+                   (k[0] - 1, k[1] - 1),
+                   (k[0] + 1, k[1] + 1),
+                   (k[0] - 1, k[1] + 1),
+                   ]
+        if v == 'L':
+            occ = 0
+            for index in indexes:
+                if d.get(index, None):
+                    if d[index] == '#':
+                        occ += 1
+
+            if occ == 0:
                 d_temp[k] = '#'
-        else:
-            indexes = [(k[0] + 1, k[1]),
-                       (k[0] - 1, k[1]),
-                       (k[0], k[1] + 1),
-                       (k[0], k[1] - 1),
-                       (k[0] + 1, k[1] - 1),
-                       (k[0] - 1, k[1] - 1),
-                       (k[0] + 1, k[1] + 1),
-                       (k[0] - 1, k[1] + 1),
-                       ]
-            if v == 'L':
-                occ = 0
-                for index in indexes:
-                    if d.get(index, None):
-                        if d[index] == '#':
-                            occ += 1
 
-                if occ == 0:
-                    d_temp[k] = '#'
-
-            if v == '#':
-                occ = 0
-                for index in indexes:
-                    if d.get(index, None):
-                        if d[index] == '#':
-                            occ += 1
-                if occ >= 4:
-                    d_temp[k] = 'L'
+        if v == '#':
+            occ = 0
+            for index in indexes:
+                if d.get(index, None):
+                    if d[index] == '#':
+                        occ += 1
+            if occ >= 4:
+                d_temp[k] = 'L'
 
     iterations += 1
 
@@ -54,56 +50,51 @@ def solve_part_1(d, iterations=0, seated=0):
     d = {**d_temp}
     solve_part_1(d, iterations, seated_temp)
 
+
 def solve_part_2(d, iterations=0, seated=0):
     d_temp = {**d}
 
-    arr = []
     for k, v in d.items():
-        arr.append(v)
-        if len(arr) > 9:
-            print(''.join(arr))
-            arr = []
-    print('----------')
-    for k, v in d.items():
-        if iterations == 0:
-            if v != '.':
+        indexes = [(1, 0),
+                   (-1, 0),
+                   (0, 1),
+                   (0, -1),
+                   (1, -1),
+                   (-1, -1),
+                   (1, 1),
+                   (-1, 1),
+                   ]
+
+        if d[k] != '.':
+            sharp = 0
+            for index in indexes:
+                crds = list(k)
+                itters = 0
+                while itters < len(data):
+                    crds[0] += index[0]
+                    crds[1] += index[1]
+
+                    if d.get(tuple(crds), None):
+                        if d[tuple(crds)] == '#':
+                            sharp += 1
+                            break
+                        if d[tuple(crds)] == 'L':
+                            break
+
+                    itters += 1
+
+            if v == 'L' and sharp == 0:
                 d_temp[k] = '#'
-        else:
-            indexes = [(k[0] + 1, k[1]),
-                       (k[0] - 1, k[1]),
-                       (k[0], k[1] + 1),
-                       (k[0], k[1] - 1),
-                       (k[0] + 1, k[1] - 1),
-                       (k[0] - 1, k[1] - 1),
-                       (k[0] + 1, k[1] + 1),
-                       (k[0] - 1, k[1] + 1),
-                       ]
-            if v == 'L':
-                occ = 0
-                for index in indexes:
-                    if d.get(index, None):
-                        if d[index] == '#':
-                            occ += 1
 
-                if occ == 0:
-                    d_temp[k] = '#'
-
-            if v == '#':
-                occ = 0
-                for index in indexes:
-                    if d.get(index, None):
-                        if d[index] == '#':
-                            occ += 1
-                if occ >= 4:
-                    d_temp[k] = 'L'
+            if v == '#' and sharp >= 5:
+                d_temp[k] = 'L'
 
     iterations += 1
-    if iterations == 3:
-        return
     seated_temp = 0
     for k, v in d_temp.items():
         if v == '#':
             seated_temp += 1
+
     if seated_temp == seated:
         print('part 2:', seated_temp)
         return
@@ -119,7 +110,7 @@ if __name__ == '__main__':
     for i_y, row in enumerate(data):
         for i_x, value in enumerate(row):
             d[(i_y, i_x)] = value
-    # solve_part_1(d)
+    solve_part_1(d)
     solve_part_2(d)
 
     # solve_part_1(data)
