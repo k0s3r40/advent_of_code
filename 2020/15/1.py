b678bbca096fd7f8b1ea121e32ae78ac3bb6f7d9
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-15 23:40:41 +0200

    Optimize pt.1

--- 2020/15/1.py
+++ 2020/15/1.py
@@ -1,41 +1,28 @@
-def solve_1(data, d, r):
-    i = len(data) + 1
+def solve_1(data, r):
+    d = {i: [] for i in range(r + 1)}
+    for index, x in enumerate(data, start=1):
+        d[x].append(index)
+    start = len(data) + 1
     last = data[-1]
-    while i <= r:
-        if len(d[last]) <= 1:
+    print(last)
+    for i in list(d.keys())[start:]:
+        if len(d[last]) == 1:
             next_digit = 0
         else:
             next_digit = last
             next_digit = d[next_digit][1] - d[next_digit][0]
-        if d.get(next_digit, None) is None:
+        if len(d[next_digit]) == 0:
             d[next_digit] = [i]
         else:
-            d[next_digit].append(i)
-            d[next_digit] = d[next_digit][-2:]
-
+            d[next_digit] = (d[next_digit][-1], i)
         last = next_digit
-
-        i += 1
     return last
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         data = [int(value) for value in f.read().split(',')]
-    d = {}
-    for index, i in enumerate(data, start=1):
-        if d.get(i, None) is None:
-            d[i] = [index]
-        else:
-            d[i].append(index)
 
-    print('Part 1:', solve_1(data, d, 2020))
+    print('Part 1:', solve_1(data, 2020))
 
-    d = {}
-    for index, i in enumerate(data, start=1):
-        if d.get(i, None) is None:
-            d[i] = [index]
-        else:
-            d[i].append(index)
-
-    print('Part 2:', solve_1(data, d, 30000000))
+    # print('Part 2:', solve_1(data, 30000000))
