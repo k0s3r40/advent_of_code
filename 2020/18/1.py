262e49f13d396cacab6db9fd2deafdc2a564eb72
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-18 12:58:06 +0200

    Solve par 1

--- 2020/18/1.py
+++ 2020/18/1.py
@@ -16,12 +16,26 @@ def solve_1(data):
                     if last_operator == '*':
                         ans *= data[index + 1]
                     last_value = ans
-    print(ans)
+    return ans
 
 
 if __name__ == '__main__':
     symbols = ['+', '-', '*', '(', ')']
     with open('input.txt') as f:
-        data = f.read()
-    # print(data)
-    # solve_1(data)
+        all_data = f.read().split('\n')
+    total = []
+    for data in all_data:
+        while True:
+            data = data.replace('(', ('\n(')).replace(')', ')\n').split('\n')
+            for index, i in enumerate(data):
+                if '(' in i and ')' in i:
+                    eq = [int(i) if i not in symbols else i for i in i.replace('(', '', ).replace(')', '').split(' ')]
+                    data[index] = str(solve_1(eq))
+
+            data = ''.join(data)
+            if '(' not in data:
+                break
+
+        data = [int(i) if i not in symbols else i for i in data.split(' ')]
+        total.append(solve_1(data))
+    print('Part 1:', sum(total))
