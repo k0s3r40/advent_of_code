3128307e1ee139edae32ffb8b83a5a6cec313a15
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-18 16:04:22 +0200

    Solve part 2

--- 2020/18/1.py
+++ 2020/18/1.py
@@ -1,5 +1,24 @@
 def solve_1(data):
     ans = 0
+    d = {}
+
+    while True:
+        forbidden_indexes = []
+        ok = False
+        for index, i in enumerate(data):
+            if index % 2 == 1:
+                if i == '+' and ok is False:
+                    d[(index - 1, index + 1)] = int(sum([data[index - 1], data[index + 1]]))
+                    data[index - 1] = int(sum([data[index - 1], data[index + 1]]))
+                    forbidden_indexes.append(index)
+                    forbidden_indexes.append(index + 1)
+                    ok = True
+
+        if forbidden_indexes == []:
+            break
+
+        data = [i for index, i in enumerate(data) if index not in forbidden_indexes]
+
 
     for index, i in enumerate(data, start=1):
         if isinstance(i, int):
@@ -31,11 +50,11 @@ if __name__ == '__main__':
                 if '(' in i and ')' in i:
                     eq = [int(i) if i not in symbols else i for i in i.replace('(', '', ).replace(')', '').split(' ')]
                     data[index] = str(solve_1(eq))
-
             data = ''.join(data)
             if '(' not in data:
                 break
 
         data = [int(i) if i not in symbols else i for i in data.split(' ')]
         total.append(solve_1(data))
+
     print('Part 1:', sum(total))
