c322bf37effd06bfe54b00a8f488d99d9c32a6a1
kostadin slavov <kostadin.slavov@tixi.bg>
2020-12-20 03:18:54 +0200

    Solve part 1 wip part 2

--- 2020/19/1.py
+++ 2020/19/1.py
@@ -1,20 +1,72 @@
-def test(s,seq): # can we produce string s using rules list seq?
-    if s == '' or seq == []: return s == '' and seq == [] # if both empty, True; if one, False
-    r = rules[seq[0]]
-    if '"' in r:
-        return test(s[1:], seq[1:]) if s[0] in r else False # strip first character, False if cannot
-    else:
-        return any(test(s, t + seq[1:]) for t in r) # expand first term in seq rules list
+import re
 
-def parse(s): # return rule pair (n,e) e.g. (2, [[2,3],[3,2]]) or (42,'"a"')
-    n,e = s.split(": ")
-    return (int(n),e) if '"' in e else (int(n), [[int(r) for r in t.split()] for t in e.split("|")])
 
-rule_text, messages = [x.splitlines() for x in open("input.txt").read().split("\n\n")]
-rules = dict(parse(s) for s in rule_text)            
-print("Part 1:", sum(test(m,[0]) for m in messages))       
+def solve(regex, messages):
+    r = re.compile(regex, re.VERBOSE)
+    valid_messages = list(filter(r.match, messages))
 
-rule_text += ["8: 42 | 42 8","11: 42 31 | 42 11 31"]
-rules = dict(parse(s) for s in rule_text)
-print(rules)
-print("Part 2:", sum(test(m,[0]) for m in messages))
+    print(len(valid_messages))
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        data, messages = f.read().split('\n\n')
+        data = data.split('\n')
+        messages = messages.split('\n')
+    # data += [f"8: {'|'.join([''.join(['42 '*(i+1)]) for i in range(50)])} ",
+    #          f"11: {'| '.join([''.join(['42 '*(i+1)+ '31 '*(i+1)]) for i in range(50)])}"
+    #          ]
+    d = {}
+
+    special = []
+    for i in data:
+        rule_id = int(i.split(':')[0])
+        conditions = i.split(':')[1].split('|')
+        d[rule_id] = ''
+        for c in conditions:
+            if '\"' in c:
+                c = c.replace('\"', '').strip()
+                d[rule_id] = c
+                special.append(str(rule_id))
+            else:
+                c = str([int(y) for y in (c.split())]).replace(']', ')').replace('[', '(').replace(',', '')
+                d[rule_id] += str(c)
+    has_looped = False
+    while True:
+        d_temp = {**d}
+
+        for i in sorted(list(d)):
+            for s in special:
+                d[i] = d[i].replace(f'({s} ', f'({d[int(s)]} ')
+                d[i] = d[i].replace(f' {s})', f' {d[int(s)]})')
+                d[i] = d[i].replace(f'({s})', f'({d[int(s)]})')
+                d[i] = d[i].replace(f' {s} ', f' {d[int(s)]} ')
+
+                is_special = True
+
+                for x in d[i].replace('(', '').replace(')', ' ').replace('[', '').replace(']', '').replace('|', '').split():
+                    try:
+                        # print(x)
+                        if x not in ['(', ')', ',', ' ', 'a', 'b', '\b', '+', '|']:
+                            isinstance(int(i), int)
+                            is_special = False
+
+                    except Exception as E:
+                        pass
+                if is_special:
+                    special.append(str(i))
+                    special = list(set(special))
+        if not re.findall('\d', str(d[0])):
+            break
+        if d_temp == d:
+            has_looped = True
+        for k, v in d.items():
+            print(k, v)
+
+    regex = d[0].replace('))((', '))|((').replace(')(', '|').replace(' ', '')
+    regex = f'^{regex}$'
+    # print(regex)
+    # Not 243
+    # part 2 not 389, 301
+    # 226
+    solve(regex, messages)
